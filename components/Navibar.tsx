df4b19be97674b4d27d1e5fc529447ec85239233
justin-aquino <justin.aquino25@gmail.com>
2023-04-26 13:41:46 -0700

    2:22:11 toggling show/hide profile added

--- components/Navibar.tsx
+++ components/Navibar.tsx
@@ -1,14 +1,20 @@
 import NavbarItem from "./NavbarItem";
-import { BsChevronDown } from "react-icons/bs";
+import AccountMenu from "./AccountMenu";
+import { BsBell, BsChevronDown, BsSearch } from "react-icons/bs";
 import MobileMenu from "./MobileMenu";
 import { useState, useCallback } from "react";
 
 const Navibar = () => {
  const [showMobileMenu, setShowMobileMenu] = useState(false);
+ const [showAccountMenu, setShowAccountMenu] = useState(false);
 
  const toggleMobileMenu = useCallback(() => {
   setShowMobileMenu((current) => !current);
  }, [showMobileMenu]);
+
+ const toggleAccountMenu = useCallback(() => {
+  setShowAccountMenu((current) => !current);
+ }, [showAccountMenu]);
  return (
   <nav className="w-full fixed z-40">
    <div className="px-4 md:px16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90">
@@ -29,6 +35,25 @@ const Navibar = () => {
      <p className="text-white text-sm"> Browse</p>
      <BsChevronDown className="text-white transition" />
      <MobileMenu visible={showMobileMenu} />
+    </div>
+    <div className="flex flex-row ml-auto gap-7 items-center">
+     <div className="text-gray-200 hover:text-gray-300 cursor-pointer">
+      <BsSearch />
+     </div>
+     <div className="text-gray-200 hover:text-gray-300 cursor-pointer">
+      <BsBell />
+     </div>
+     <div
+      onClick={toggleAccountMenu}
+      className="flex flex-row items-center gap-2 cursor-pointer relative"
+     >
+      <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
+       <img src="/images/default-red.png" alt="profile image" />
+      </div>
+
+      <BsChevronDown className="text-white transition" />
+      <AccountMenu visible={showAccountMenu} />
+     </div>
     </div>
    </div>
   </nav>
