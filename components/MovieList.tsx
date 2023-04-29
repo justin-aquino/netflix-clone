c1e0ce7b82b5341462b817169a5119d67b8528c6
justin-aquino <justin.aquino25@gmail.com>
2023-04-28 17:55:12 -0700

    displayiing movie list thumbnail

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { isEmpty } from "lodash";
+import MovieCard from "./MovieCard";
 
 interface MovieListProps {
  data: Record<string, any>[];
@@ -11,20 +12,23 @@ const MovieList: React.FC<MovieListProps> = ({ data, title }) => {
   return null;
  }
 
- const mappedData = data.map((item) => {
-  return (
-   <p className="text-white" key={item.id}>
-    movie
-   </p>
-  );
- });
+ //  const mappedData = data.map((movie) => {
+ //   return <MovieCard data={movie} key={movie.id} />;
+ //  });
  return (
   <div className="px-4 md:px-12 mt-4 space-y-8">
-   <div className="grid grid-cols-4 gap-2">
-    <p className="text-white text-md md:text-xl lg:text-2xl font-semibold">
+   <div>
+    <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">
      {title}
     </p>
-    <div>{mappedData}</div>
+    {/* <div className="grid grid-cols-4 gap-2">{mappedData}</div> */}
+    <div className="grid grid-cols-4 gap-2">
+     {data.map((movie) => (
+      <div key={movie.id}>
+       <MovieCard data={movie} />
+      </div>
+     ))}
+    </div>
    </div>
   </div>
  );
