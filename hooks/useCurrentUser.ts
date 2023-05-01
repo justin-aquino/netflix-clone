782b5494e7707333e14580f63f94c81d797c2fcc
justin-aquino <justin.aquino25@gmail.com>
2023-05-01 13:39:59 -0700

    adding useMovie hook

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -1,8 +1,8 @@
-import useSwr from "swr";
+import useSWR from "swr";
 import fetcher from "@/lib/fetcher";
 
 const useCurrentUser = () => {
- const { data, error, isLoading, mutate } = useSwr("/api/current", fetcher); //useSWR is a fetcher, a state manager.
+ const { data, error, isLoading, mutate } = useSWR("/api/current", fetcher); //useSWR is a fetcher, a state manager.
  return {
   data,
   error,
