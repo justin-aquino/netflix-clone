354aef8fefb15fcee75d6fa052e2e893fad6faad
justin-aquino <justin.aquino25@gmail.com>
2023-05-02 18:02:53 -0700

    implementing open info modal on smaller movie cards

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BsFillPlayFill } from "react-icons/bs";
 import FavoriteBtn from "./FavoriteBtn";
 import { useRouter } from "next/router";
+import { BiChevronDown } from "react-icons/bi";
+import useInfoModal from "@/hooks/useInfoModal";
 
 interface MovieCardProps {
  data: Record<string, any>;
@@ -9,6 +11,7 @@ interface MovieCardProps {
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
  const router = useRouter();
+ const { openModal } = useInfoModal();
 
  return (
   <div className="group bg-zinc-900 col-span relative h-[12vw]">
@@ -84,6 +87,25 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
        <BsFillPlayFill size={30} />
       </button>
       <FavoriteBtn movieId={data?.id} />
+      <button
+       className="
+        ml-auto 
+        group/item 
+        w-6 h-6 
+        lg:w-10 
+        lg:h-10 
+        border-white 
+        border-2 
+        rounded-full 
+        flex 
+        justify-center 
+        items-center 
+        transition 
+        hover:border-neutral-300"
+       onClick={() => openModal(data?.id)}
+      >
+       <BiChevronDown className="text-white group-hover/item:text-neutral-300" />
+      </button>
      </div>
      <p className="text-green-400 font-semibold mt-4">
       New <span className="text-white"> 2023</span>
